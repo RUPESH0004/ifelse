2653049c81b6fa787d92cf05c6f1ca7b49e02d36
RUPESH0004 <rj9082356865@gmail.com>
2023-12-01 14:07:23 +0530

    the if else stetment

---
+++ if.cpp
@@ -0,0 +1,19 @@
+// find the bigger no
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter the num1:" << endl;
+    cin >> num1;
+    cout << "Enter the num2:" << endl;
+    cin >> num2;
+    if (num1 > num2)
+    {
+        cout << "the num 1  is greater than num2";
+    }
+    else
+    {
+        cout << "The num1 is smaller than num2";
+    }
+}
